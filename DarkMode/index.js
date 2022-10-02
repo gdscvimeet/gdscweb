91bdf408e274c17e63a7a3966800a160d407ef5a
bandya2003 <61108642+bandya2003@users.noreply.github.com>
2022-10-02 17:29:22 +0530

    Created Index.js File
    For adding Dark Mode Functionality for The Web Page

---
+++ DarkMode/index.js
@@ -0,0 +1,12 @@
+<script src="darkmode.js"></script>
+<script>
+  var options = {
+    light: "light.css",
+    dark: "dark.css",
+    startAt: "23:00",
+    endAt: "06:00",
+    checkSystemScheme: true,
+    saveOnToggle: true
+  };
+  var DarkMode = new DarkMode(options);
+</script>
